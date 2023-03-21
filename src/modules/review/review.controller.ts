2cddeb2312218e197154c85c9ae86697326ccfba
kyoongdev <9898junjun2@gmail.com>
2023-03-21 17:32:10 +0900

    feat(review) : comment api

--- src/modules/review/review.controller.ts
+++ src/modules/review/review.controller.ts
@@ -1,10 +1,10 @@
 import { Body, Controller, Delete, Param, Patch, Post, UseInterceptors } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { User } from '@prisma/client';
-import { ResponseWithIdDTO } from 'common';
+import { EmptyResponseDTO, ResponseWithIdDTO } from 'common';
 import { Auth, RequestApi, ResponseApi } from 'kyoongdev-nestjs';
 import { JwtAuthGuard, ReqUser, ResponseWithIdInterceptor, Role, RoleInterceptorAPI } from 'utils';
-import { CreateReviewDTO, UpdateReviewDTO } from './dto';
+import { CreateReviewCommentDTO, CreateReviewDTO, UpdateReviewDTO } from './dto';
 import { ReviewService } from './review.service';
 
 @ApiTags('리뷰')
@@ -94,5 +94,89 @@ export class ReviewController {
   )
   async deleteReview(@Param('id') id: string, @ReqUser() user: User) {
     return await this.reviewService.deleteReview(id, user.id);
+  }
+
+  @Post(':id/comments')
+  @ApiOperation({
+    summary: '[서비스] 리뷰 댓글 생성',
+    description: '리뷰에 댓글을 생성합니다. 유저만 사용할 수 있습니다.',
+  })
+  @Auth(JwtAuthGuard)
+  @UseInterceptors(RoleInterceptorAPI(Role.USER))
+  @UseInterceptors(ResponseWithIdInterceptor)
+  @RequestApi({
+    params: {
+      name: 'id',
+      type: 'string',
+      required: true,
+      description: '리뷰의 id',
+    },
+    body: {
+      type: CreateReviewCommentDTO,
+    },
+  })
+  @ResponseApi(
+    {
+      type: ResponseWithIdDTO,
+    },
+    201
+  )
+  async createReviewComment(@Param('id') id: string, @ReqUser() user: User, @Body() body: CreateReviewCommentDTO) {
+    return await this.reviewService.createReviewComment(id, user.id, body);
+  }
+
+  @Patch('comments/:id')
+  @ApiOperation({
+    summary: '[서비스] 리뷰 댓글 수정',
+    description: '리뷰에 댓글을 수정합니다. 본인만 사용할 수 있습니다.',
+  })
+  @Auth(JwtAuthGuard)
+  @UseInterceptors(RoleInterceptorAPI(Role.USER))
+  @UseInterceptors(ResponseWithIdInterceptor)
+  @RequestApi({
+    params: {
+      name: 'id',
+      type: 'string',
+      required: true,
+      description: '댓글의  id',
+    },
+    body: {
+      type: CreateReviewCommentDTO,
+    },
+  })
+  @ResponseApi(
+    {
+      type: EmptyResponseDTO,
+    },
+    204
+  )
+  async updateReviewComment(@Param('id') id: string, @ReqUser() user: User, @Body() body: CreateReviewCommentDTO) {
+    await this.reviewService.updateReviewComment(id, user.id, body);
+  }
+
+  @Delete('comments/:id')
+  @ApiOperation({
+    summary: '[서비스] 리뷰 댓글 삭제',
+    description: '리뷰에 댓글을 삭제합니다. 본인만 사용할 수 있습니다.',
+  })
+  @Auth(JwtAuthGuard)
+  @UseInterceptors(RoleInterceptorAPI(Role.USER))
+  @UseInterceptors(ResponseWithIdInterceptor)
+  @RequestApi({
+    params: {
+      name: 'id',
+      type: 'string',
+      required: true,
+      description: '댓글의  id',
+    },
+  })
+  @ResponseApi(
+    {
+      type: EmptyResponseDTO,
+    },
+    204
+  )
+  async deleteReviewComment(@Param('id') id: string, @ReqUser() user: User) {
+    await this.reviewService.deleteReviewComment(id, user.id);
   }
 }
