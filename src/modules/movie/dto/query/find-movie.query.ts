17eaef13849ce7f0178b6abea2e75a10e1378984
kyoongdev <9898junjun2@gmail.com>
2023-04-04 10:10:56 +0900

    feat(review) : my review

--- src/modules/movie/dto/query/find-movie.query.ts
+++ src/modules/movie/dto/query/find-movie.query.ts
@@ -1,5 +1,5 @@
 import { PagingDTO, Property } from 'kyoongdev-nestjs';
 export class FindMovieQuery extends PagingDTO {
-  @Property({ apiProperty: { type: 'string', nullable: true, description: '영화 제목' } })
+  @Property({ apiProperty: { type: 'string', nullable: true, description: '영화 제목 | 감독 이름 | 배우 이름|' } })
   title?: string;
 }
