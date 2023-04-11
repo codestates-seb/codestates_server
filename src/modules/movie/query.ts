6332dc649e35695dc19c417e974d454e5e0501ac
kyoongdev <9898junjun2@gmail.com>
2023-04-11 13:06:31 +0900

    feat : movie joined table order by

--- src/modules/movie/query.ts
+++ src/modules/movie/query.ts
@@ -5,20 +5,40 @@ export const movieIncludeOption: Prisma.MovieInclude = {
     include: {
       actor: true,
     },
+    orderBy: {
+      actor: {
+        name: 'desc',
+      },
+    },
   },
   movieGenres: {
     include: {
       genre: true,
+    },
+    orderBy: {
+      genre: {
+        name: 'desc',
+      },
     },
   },
   movieStaffs: {
     include: {
       staff: true,
     },
+    orderBy: {
+      staff: {
+        name: 'desc',
+      },
+    },
   },
   movieCategories: {
     include: {
       category: true,
+    },
+    orderBy: {
+      category: {
+        name: 'desc',
+      },
     },
   },
   reviews: true,
