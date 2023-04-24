cb7167f8a4f727155f25f00a75c03205e50ded8f
kyoongdev <9898junjun2@gmail.com>
2023-04-24 20:50:13 +0900

    feat(db) : migrate

--- src/seed.ts
+++ src/seed.ts
@@ -1,100 +1,58 @@
 import { PrismaClient } from '@prisma/client';
-import axios from 'axios';
+import mysql from 'mysql';
+const connection = mysql.createConnection({
+  host: 'blog-db.cbb6a8d1ki52.ap-northeast-2.rds.amazonaws.com',
+  user: 'kyoong',
+  password: 'cecj8bNpvHAyuObTjOWq',
+  database: 'codestates',
+  port: 3306,
+});
 
 const database = new PrismaClient();
 
 (async () => {
-  const movie = await axios.get(
-    'http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=8Z3T9WBWD7IMW1MW3HG9&detail=Y&startCount=0&listCount=200&nation=대한민국&ratedYn=Y&releaseDts=20200101&type=극영화',
-    {}
-  );
-  for (const row of movie.data.Data) {
-    for (const item of row.Result) {
-      const isExist = await database.movie.findFirst({
-        where: {
-          title: item.title,
+  connection.connect();
+  connection.query('Delete from Movie');
+  connection.query('Delete from User');
+
+  connection.query('SELECT * from Movie', async (error, rows, fields) => {
+    if (error) throw error;
+    rows.forEach(async (row: any) => {
+      await database.movie.create({
+        data: row,
+      });
+    });
+  });
+
+  connection.query('SELECT * from User', async (error, rows, fields) => {
+    if (error) throw error;
+    rows.forEach(async (row: any) => {
+      await database.user.create({
+        data: {
+          id: row.id,
+          name: row.name,
+          birth: row.birth,
+          email: row.email,
+          nickname: row.nickname,
+          password: row.password,
+          description: row.description,
+          gender: row.gender,
+          userType: row.userType,
+          isPublic: row.isPublic === 1 ? true : false,
+          isLikeView: row.isLikeView === 1 ? true : false,
+          isReviewView: row.isReviewView === 1 ? true : false,
+          isPreferenceView: row.isPreferenceView === 1 ? true : false,
+          profileImage: row.profileImage,
+          createdAt: row.createdAt,
+          updatedAt: row.updatedAt,
         },
       });
-      if (!isExist) {
-        const actors = await Promise.all(
-          item.actors.actor.slice(0, 10).map(async (actor: any) => {
-            const isExist = await database.actor.findFirst({
-              where: {
-                name: actor.actorNm,
-              },
-            });
-            if (!isExist) {
-              const newActor = await database.actor.create({
-                data: {
-                  name: actor.actorNm,
-                },
-              });
-              return newActor.id;
-            }
-            return isExist.id;
-          })
-        );
-        const staffs = await Promise.all(
-          item.staffs.staff.slice(0, 10).map(async (staff: any) => {
-            const isExist = await database.staff.findFirst({
-              where: {
-                name: staff.staffNm,
-              },
-            });
-            if (!isExist) {
-              const newStaff = await database.staff.create({
-                data: {
-                  name: staff.staffNm,
-                  role: staff.staffRoleGroup,
-                },
-              });
-              return newStaff.id;
-            }
-            return isExist.id;
-          })
-        );
-        const movie = await database.movie.create({
-          data: {
-            title: item.title,
-            company: item.company,
-            rating: item.rating,
-            plot: item.plots.plot.filter((p) => p.plotLang === '한국어')[0]?.plotText || '',
-            postImage: item.posters.split('|')[0] || '',
-            releasedAt: item.repRlsDate,
-            runtime: item.runtime,
-          },
-        });
-        for (const actorId of actors) {
-          const isExist = await database.movieActor.findFirst({
-            where: {
-              actorId,
-              movieId: movie.id,
-            },
-          });
-          if (!isExist)
-            await database.movieActor.create({
-              data: {
-                actorId,
-                movieId: movie.id,
-              },
-            });
-        }
-        for (const staffId of staffs) {
-          const isExist = await database.movieStaff.findFirst({
-            where: {
-              staffId,
-              movieId: movie.id,
-            },
-          });
-          if (!isExist)
-            await database.movieStaff.create({
-              data: {
-                staffId,
-                movieId: movie.id,
-              },
-            });
-        }
-      }
-    }
-  }
+    });
+  });
+
+  const users = await database.user.findMany();
+
+  const movies = await database.movie.findMany();
+
+  connection.end();
 })();
